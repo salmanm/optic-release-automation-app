8c6ec07605a071c2e124f1afd8aea9939699964a
Davide Roffo <davide.roffo@nearform.com>
2022-12-07 11:54:59 +0100

    feat: add generateReleaseNotes and releaseNotes

--- index.test.js
+++ index.test.js
@@ -152,3 +152,53 @@ test('create release with no target specified', async t => {
 
   t.same(response.statusCode, 200)
 })
+
+test('create release with releaseNotes specified', async t => {
+  const mockedRepo = { repo: 'smn-repo', owner: 'salmanm' }
+
+  const getAccessTokenStub = sinon.stub().resolves('some-token')
+  const createDraftReleaseStub = sinon.stub().resolves('some-token')
+
+  const app = await setup(async server => {
+    server.addHook('onRequest', async req => {
+      req.auth = { ...mockedRepo }
+    })
+    server.decorate('github', {
+      getAccessToken: getAccessTokenStub,
+      createDraftRelease: createDraftReleaseStub,
+    })
+  })
+
+  const response = await app.inject({
+    method: 'POST',
+    headers: {
+      authorization: 'token gh-token',
+      'content-type': 'application/json',
+    },
+    url: '/release',
+    body: JSON.stringify({
+      version: 'v9.9.9',
+      generateReleaseNotes: true,
+      releaseNotes: 'my release notes',
+    }),
+  })
+
+  t.same(getAccessTokenStub.callCount, 1)
+  t.ok(getAccessTokenStub.calledWithExactly('salmanm', 'smn-repo'))
+
+  t.same(createDraftReleaseStub.callCount, 1)
+  t.ok(
+    createDraftReleaseStub.calledWithExactly(
+      {
+        version: 'v9.9.9',
+        generateReleaseNotes: true,
+        releaseNotes: 'my release notes',
+        owner: 'salmanm',
+        repo: 'smn-repo',
+      },
+      'some-token'
+    )
+  )
+
+  t.same(response.statusCode, 200)
+})
