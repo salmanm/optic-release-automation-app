ca83e2a88b5944e08316885a95e1512a18abf706
Nick Da Silva <nickdasilva@protonmail.com>
2024-09-24 16:40:46 +0100

    fix: use dynamic import of @octokit/rest to handle differences with E… (#453)
    * fix: use dynamic import of @octokit/rest to handle differences with ESM and Commonjs
    
    * chore: update app to ESM. App appropriate test for import/request change.
    
    * chore: set node version in .npmrc used in workflows to 20. Add node to cd.yml workflow
    
    * chore: set .npmrc to lts/*

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-'use strict'
+import Fastify from 'fastify';
 
-const Fastify = require('fastify')
+import githubRoutes from './lib/github.js'
+import authRoutes from './lib/auth.js'
+import routes from './lib/routes.js'
+
+import config from './lib/config.js'
 
 const fastify = Fastify({
   logger: true,
 })
 
-const config = require('./lib/config')
 
 fastify
-  .register(require('./lib/github'), config)
-  .register(require('./lib/auth'), config)
-  .register(require('./lib/routes'), config)
+  .register(githubRoutes, config)
+  .register(authRoutes, config)
+  .register(routes, config)
 
 fastify.listen({ port: config.PORT, host: '0.0.0.0' })
+
+export default fastify
